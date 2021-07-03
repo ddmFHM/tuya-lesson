75f5ace422282521ea09ede9af89f7fc6a1ab329
ddmFHM <ddmFHM@163.com>
2021-07-03 12:45:15 +0800

    redux源码

--- shopping/src/store/actions/product.actions.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from 'redux-actions'
-
-/* 向服务端发生请求 获取商品列表数据 */
-export const loadProducts = createAction('load_products')
-/* 将服务端返回的商品列表数据保存到本地的store对象中 */
-export const saveProducts = createAction('save_products')
